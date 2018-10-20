90d94aa512b1730a9f3aa60141aad2b9f9f31653
Gott50 <xxGott50xx@gmail.com>
2018-10-20 17:35:08 +0200

    search Questions

--- src/bot/answers.py
+++ src/bot/answers.py
@@ -10,5 +10,16 @@ class Answers:
         wb = openpyxl.load_workbook(location)
         self.sheet = wb[wb.sheetnames[0]]
 
-    def get(self):
-        print(self.sheet['A1'])
+    def get(self, question, options=[]):
+        print(self.sheet['A'])
+        for cell in self.sheet['A']:
+            print(cell.value)
+            if(cell.value == question):
+                return self.get_answer(options)
+        return self.new_entry(question, options)
+
+    def get_answer(self, options):
+        return []
+
+    def new_entry(self, quesiton, options):
+        return options
