446450843067944e8e1d64e9eb144a17975ae9d4
Gott50 <xxGott50xx@gmail.com>
2018-10-21 09:28:40 +0200

    refactor

--- src/test_answers.py
+++ src/test_answers.py
@@ -3,4 +3,4 @@ from bot import Answers
 answers = Answers('Braindump (DevOps) Azure DevOps Assessment (18473).xlsx')
 
 print("Answer: %s" %
-      answers.get(question='q', options=['a','b','c']))
+      answers.get(question='q', options=['a', 'b', 'c']))
