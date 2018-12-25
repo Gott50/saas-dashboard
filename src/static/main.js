c5f24cee5e26baa892874a97570e70e13d2204d9
Gott50 <xxGott50xx@gmail.com>
2018-12-25 17:57:54 +0100

    submit <form> via AJAX

---
+++ src/static/main.js
@@ -0,0 +1,44 @@
+// custom javascript
+
+$( document ).ready(() => {
+  console.log('Sanity Check!');
+});
+
+function submit() {
+  console.log($('form').serialize())
+  $.ajax({
+    url: '/user',
+    data: $('form').serialize(),
+    method: 'POST'
+  })
+      .done((res) => {
+        getStatus(res.data.task_id)
+      })
+      .fail((err) => {
+        console.log(err)
+      });
+};
+
+function getStatus(taskID) {
+  $.ajax({
+    url: `/tasks/${taskID}`,
+    method: 'GET'
+  })
+  .done((res) => {
+    const html = `
+      <tr>
+        <td>${res.data.task_id}</td>
+        <td>${res.data.task_status}</td>
+        <td>${res.data.task_result}</td>
+      </tr>`
+    $('#tasks').prepend(html);
+    const taskStatus = res.data.task_status;
+    if (taskStatus === 'finished' || taskStatus === 'failed') return false;
+    setTimeout(function() {
+      getStatus(res.data.task_id);
+    }, 1000);
+  })
+  .fail((err) => {
+    console.log(err)
+  });
+}
