8feba5b84e01d455418bf31350bc25474f51dd09
Gott50 <xxGott50xx@gmail.com>
2019-03-31 13:41:21 +0200

    fix stopped_shotdown.sh

--- src/bot_scripts/stopped_shutdown.sh
+++ src/bot_scripts/stopped_shutdown.sh
@@ -1,11 +1,12 @@
 #!/bin/sh
+IMAGE=saas-bot
 
-RUNNING="$(sudo docker ps --format 'table {{.Image}}' -f STATUS=running | grep -w instagramtools)"
+RUNNING="$(sudo docker ps --format 'table {{.Image}}' -f STATUS=running | grep -w $IMAGE)"
 
 while [ "$RUNNING" ]
 do
     sleep 30
-    RUNNING="$(sudo docker ps --format 'table {{.Image}}' -f STATUS=running | grep -w instagramtools)"
+    RUNNING="$(sudo docker ps --format 'table {{.Image}}' -f STATUS=running | grep -w $IMAGE)"
 done
 
 echo SHUTDOWN because Bot stopped
